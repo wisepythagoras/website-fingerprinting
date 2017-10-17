ac235c72e547498a22bb7e7b27e98e763467c316
conmarap <conmarap@gmail.com>
2017-10-16 20:34:23 -0400

    Some changes in the dummy classifier

--- dummy_classifier.py
+++ dummy_classifier.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 print("This is just a dummy classifier that simulates packet sizes in order")
 
@@ -20,8 +22,19 @@ streams = [
 
 labels = [0, 0, 0, 1, 1, 1]
 
-clf = tree.DecisionTreeClassifier()
+# We can either do a decision tree or a KNN classifier.
+# clf = tree.DecisionTreeClassifier()
+clf = KNeighborsClassifier()
 clf = clf.fit(streams, labels)
 
-print(clf.predict([[123, 191, 83, 262, 154, 140]]))
-print(clf.predict([[139, 155, 139, 157, 162, 140]]))
+x_test = [
+    [123, 191, 83, 262, 154, 140],
+    [139, 155, 139, 157, 162, 140],
+]
+
+print(clf.predict([x_test[0]]))
+print(clf.predict([x_test[1]]))
+
+predictions = clf.predict(x_test)
+
+print(accuracy_score([1, 0], predictions))
