3d170827f455aed2b0b5e9da0fe5a016a3cbab85
Constantine <conmarap@gmail.com>
2017-11-30 09:23:40 -0500

    Fix typo

--- capture.sh
+++ capture.sh
@@ -14,9 +14,9 @@ fname="$1-$(date +'%m-%d-%y_%T')"
 
 if [ ! -z "$2" ]; then
     fname="$fname-$2"
-    echo "PCAPs in ./pcaps/$1: " $(ls -ltr ~/pcaps/$1/ | grep "$2.pcap$" | wc -l)
+    echo "PCAPs in ./pcaps/$1: " $(ls -ltr ./pcaps/$1/ | grep "$2.pcap$" | wc -l)
 else
-    echo "PCAPs in ./pcaps/$1: " $(ls -ltr ~/pcaps/$1/ | grep ".pcap$" wc -l)
+    echo "PCAPs in ./pcaps/$1: " $(ls -ltr ./pcaps/$1/ | grep ".pcap$" wc -l)
 fi
 
 sudo tcpdump -vv -x -X -i eth0 -A tcp and port not 22 -w ./pcaps/$1/$fname.pcap
