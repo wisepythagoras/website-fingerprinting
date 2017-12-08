f311cfdad23b2aa40a57d6c902c442065f7e0a23
Constantine Apostolou <conmarap@gmail.com>
2017-12-07 20:13:32 -0500

    Make changes to add more parameters to classifier

--- gather_and_train.py
+++ gather_and_train.py
@@ -5,12 +5,19 @@ import re
 import sys
 import json
 import dpkt
+import socket
 import random
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.externals import joblib
+
+
+def inet_to_str(inet):
+    """ Convert inet object to a string """
+
+    return socket.inet_ntop(socket.AF_INET, inet)
 
 
 def shuffle(x, y):
@@ -43,17 +50,86 @@ def read_pcap_file(file):
     pcap = dpkt.pcap.Reader(fp)
 
     # This is the array that will contain all the packet sizes.
-    sizes = [0] * 30
+    sizes = [0] * 40
     i = 0
+
+    # Hold the addresses of the outgoing agent.
+    outgoing_addr = None
+
+    outgoing_packets = 0
+    incoming_packets = 0
+    total_number_of_packets = 0
+
+    # This will contain the total size of the incoming packets.
+    incoming_size = 0
 
     # Loop through all the packets and save the sizes.
     for ts, buf in pcap:
-        if i >= 30:
-            break
+        packet_size = len(buf)
 
-        sizes[i] = len(buf)
+        if i < 40:
+            # Add the size to the array.
+            sizes[i] = packet_size
 
-        i += 1
+            # Increment the index.
+            i += 1
+
+        # Parse the Ethernet packet.
+        eth = dpkt.ethernet.Ethernet(buf)
+
+        # Parse the IP packet.
+        ip = eth.data
+
+        # Get the source addresses.
+        src = inet_to_str(ip.src)
+
+        if total_number_of_packets == 0:
+            # Get the address of the outgoing agents. The target user is the
+            # outgoing agent, and the incoming packets are the server/website.
+            outgoing_addr = src
+            outgoing_packets += 1
+
+        elif src == outgoing_addr:
+            # Increment the outgoing packets.
+            outgoing_packets += 1
+
+        else:
+            # Increment the incoming packets.
+            incoming_packets += 1
+
+            # Increment the size of the incoming packets.
+            incoming_size += packet_size
+
+        # Increment the total amount of packets.
+        total_number_of_packets += 1
+
+    # Get the ratio.
+    ratio = float(incoming_packets) / (outgoing_packets if outgoing_packets != 0 else 1)
+
+    # Print some details.
+    print "OUT: {}, IN: {}, TOTAL: {}, RATIO: {}".format(\
+        outgoing_packets, incoming_packets, total_number_of_packets, ratio)
+
+    # Reverse the array to append the other information.
+    sizes.reverse()
+
+    # Add the ratio of incoming to outgoing packets.
+    sizes.append(ratio)
+
+    # Add the number of incoming packets.
+    sizes.append(incoming_packets)
+
+    # Add the number of outgoing packets.
+    sizes.append(outgoing_packets)
+
+    # Add the number of total packets.
+    sizes.append(total_number_of_packets)
+
+    # Add the total size of the incoming packets.
+    sizes.append(incoming_size)
+
+    # Reverse the array again so that the sizes are in order.
+    sizes.reverse()
 
     # Finally return the sizes.
     return sizes
@@ -80,7 +156,7 @@ def train(streams, labels):
     print("Testing size:  {}".format(stream_amount - training_size))
 
     # Initialize the classifier.
-    clf = MultinomialNB()
+    clf = KNeighborsClassifier()
 
     # Now lets train our KNN classifier.
     clf = clf.fit(training_x, training_y)
@@ -147,6 +223,8 @@ with open('config.json') as fp:
         # Try to read the classifier.
         classifier = joblib.load("./classifier-nb.dmp")
         i = 0
+        right = 0
+        wrong = 0
 
         for stream in streams:
             # Run the prediction.
