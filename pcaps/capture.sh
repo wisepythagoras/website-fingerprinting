17a19ec1d972d8e6e0348979e286b940f8eab424
Rhenium <chigakuishi@users.noreply.github.com>
2019-01-19 22:19:57 +0900

    Update capture.sh
    insert `|`

--- pcaps/capture.sh
+++ pcaps/capture.sh
@@ -16,7 +16,7 @@ if [ ! -z "$2" ]; then
     fname="$fname-$2"
     echo "PCAPs in ./pcaps/$1: " $(ls -ltr ./pcaps/$1/ | grep "$2.pcap$" | wc -l)
 else
-    echo "PCAPs in ./pcaps/$1: " $(ls -ltr ./pcaps/$1/ | grep ".pcap$" wc -l)
+    echo "PCAPs in ./pcaps/$1: " $(ls -ltr ./pcaps/$1/ | grep ".pcap$" | wc -l)
 fi
 
 sudo tcpdump -vv -x -X -i eth0 -A tcp and port not 22 -w ./pcaps/$1/$fname.pcap
